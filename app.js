9cbcb876fc0201d91166aac6f0b3bfd48c5367d0
anttikor <a.kortelainen@gmail.com>
2023-10-16 13:42:55 +0300

    update

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ const fs = require('fs');
 const path = require('path');
 
 // Initialize HTML content
-let htmlContent = '<!DOCTYPE html><html><head><title>Screenshots</title></head><body>';
+let htmlContent = `<!DOCTYPE html>
+<html>
+  <head>
+    <title>Screenshots</title>
+    <link rel="stylesheet" type="text/css" href="../styles.css">
+  </head>
+  <body>`;
 
 // Read pages and URL from .env file
 const subpages = process.env.SUBPAGES ? process.env.SUBPAGES.split(',') : [];
@@ -13,8 +19,8 @@ const baseURL = process.env.BASE_URL || '';
 
 // Create a root directory for screenshots if it doesn't exist
 const screenshotsDir = 'screenshots';
-if (!fs.existsSync(screenshotsDir)){
-    fs.mkdirSync(screenshotsDir);
+if (!fs.existsSync(screenshotsDir)) {
+  fs.mkdirSync(screenshotsDir);
 }
 
 (async () => {
@@ -30,12 +36,19 @@ if (!fs.existsSync(screenshotsDir)){
     const sanitizedPageName = pageName.replace('/', '-');
     const pageFolder = path.join(screenshotsDir, sanitizedPageName || 'index');
 
-    if (!fs.existsSync(pageFolder)){
-        fs.mkdirSync(pageFolder);
+    if (!fs.existsSync(pageFolder)) {
+      fs.mkdirSync(pageFolder);
     }
 
     const page = await browser.newPage();
-    await page.goto(`${baseURL}/${pageName}`, { waitUntil: 'networkidle2' });  // Wait until the network is idle
+    await page.goto(`${baseURL}/${pageName}`, { waitUntil: 'domcontentloaded' });  // Wait until the DOM content is loaded
+
+    // Wait for a certain period to ensure all resources are loaded
+    await page.waitForTimeout(2000); // Adjust this time as needed
+
+    // Add page title to HTML content
+    const pageTitle = await page.title();
+    htmlContent += `<h1>${pageTitle}</h1>`;
 
     for (const deviceName in devices) {
       currentView++;
@@ -43,13 +56,12 @@ if (!fs.existsSync(screenshotsDir)){
 
       const device = devices[deviceName];
       await page.emulate(device);
-
       // Take a screenshot and save it
       const screenshotPath = path.join(pageFolder, `${deviceName}.png`);
       await page.screenshot({ path: screenshotPath, fullPage: true });
 
       // Add to HTML content
-      htmlContent += `<h1>${deviceName}</h1><img src="${path.relative(screenshotsDir, screenshotPath)}" alt="${deviceName}">`;
+      htmlContent += `<h2>${deviceName}</h2><img src="${path.relative(screenshotsDir, screenshotPath)}" alt="${deviceName}">`;
     }
 
     await page.close();
